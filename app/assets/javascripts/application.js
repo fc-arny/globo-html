b001a9dd73ce0bf593fefac1f5c5c69ca0db6d8f
July <mochalova.yuliya.s@gmail.com>
2013-05-31 18:27:20 +0400

    Проверочка2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,15 @@
-// jQuery -------------------------
-//---------------------------------
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
 //= require jquery
 //= require jquery_ujs
-//= require hamlcoffee
-
-// Backbone Lib -------------------
-//---------------------------------
-//= require underscore
-//= require json2
-//= require backbone
-//= require backbone-support
-
-// Core ---------------------------
-//---------------------------------
-//= require_tree ./common
-//= require_directory ./sap
-//= require_tree ./sap/plugins
-
-// Backbone App -------------------
-//---------------------------------
-//= require_tree ./sap/models
-//= require_tree ./sap/collections
-//= require_tree ./sap/views
-//= require_tree ./sap/routers
-//= require_tree ../templates
+//= require_tree .
