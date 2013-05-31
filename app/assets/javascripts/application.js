121a85f754380e5306d735d338b5e11d2d02797b
July <mochalova.yuliya.s@gmail.com>
2013-05-31 17:19:23 +0400

    Проверочка

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// jQuery -------------------------
+//---------------------------------
+//= require jquery
+//= require jquery_ujs
+//= require hamlcoffee
+
+// Backbone Lib -------------------
+//---------------------------------
+//= require underscore
+//= require json2
+//= require backbone
+//= require backbone-support
+
+// Core ---------------------------
+//---------------------------------
+//= require_tree ./common
+//= require_directory ./sap
+//= require_tree ./sap/plugins
+
+// Backbone App -------------------
+//---------------------------------
+//= require_tree ./sap/models
+//= require_tree ./sap/collections
+//= require_tree ./sap/views
+//= require_tree ./sap/routers
+//= require_tree ../templates
