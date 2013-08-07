390ba09d6b509da831786d7af18a915964154d25
Artur Moroz <morozarturv@gmail.com>
2013-08-07 11:34:27 +0300

    +grunt

---
+++ Gruntfile.js
@@ -0,0 +1,158 @@
+'use strict';
+var path = require( 'path' );
+var lrSnippet = require( 'grunt-contrib-livereload/lib/utils' ).livereloadSnippet;
+var folderMount = function folderMount( connect, point ) {
+	return connect.static( path.resolve( point ) );
+};
+
+
+module.exports = function(grunt) {
+
+grunt.initConfig({
+	connect: {
+		livereload: {
+			options: {
+				port: 9001,
+				base: 'production',
+				middleware: function( connect, options ) {
+					return [lrSnippet, folderMount( connect, options.base )]
+				}
+			}
+		}
+	},
+	csso: {
+		dist: {
+			src: 'src/css/screen.css',
+			dest:'production/css/screen.min.css'
+		}
+	},
+	compass: {
+		dist: {
+			options: {
+				basePath: 'src/',
+				config: 'src/config.rb',
+				cssDir: 'css',
+				environment: 'production'
+			}
+		}
+	},
+	copy: {
+		main: {
+			files: [
+				{expand: true, cwd: 'src/img/', src: ['**'], dest: 'production/img'}, // copy all the pics
+				//{expand: true, cwd: 'src/sass', src: ['**'], dest: 'production/sass'}, //im not sure we need that
+				{expand: true, cwd: 'src/css', src: ['**'], dest: 'production/css'},
+				{expand: true, cwd: 'src/js', src: ['**'], dest: 'production/js'}
+			]
+		}
+	},
+	includereplace: {
+		dist: {
+			options: {
+				// Global variables available in all files
+				// globals: {
+				//  var1: 'one',
+				//  var2: 'two',
+				//  var3: 'three'
+				// },
+				// Optional variable prefix & suffix, defaults as shown
+				prefix: '@@',
+				suffix: ''
+			},
+			// Files to perform replacements and includes with
+			src: 'src/*.html',
+			// Destinaion directory to copy files to
+			dest: 'production/'
+		}
+	},
+	concat: {
+		dist: {
+			'src': [
+				'src/js/*.js'
+			],
+			dest: 'production/js/app.js'
+		}
+	},
+	styleguide: {
+		styledocco: {
+			options: {
+				framework: {
+					name: 'styledocco'
+				},
+				template: {
+					// src: 'path/to/templates',
+					include: ['production/css/screen.css','production/js/app.js']
+				},
+				name: 'MySite Style Guide'
+			},
+			files: {
+				'production/docs': 'src/sass/*.sass'
+			}
+		},
+	},
+	// min: {
+	// 	dist: {
+	// 		'src': ['production/js/app.js'],
+	// 		'dest': 'production/js/app.min.js'
+	// 	}
+	// },
+	imagemin: {
+		dist: {
+			options: {
+				optimizationLevel: 3
+			},
+			files: {
+				'production/img/*.png': 'src/img/*.png',
+				//'production/img/*.jpg': 'src/img/*.jpg'
+			}
+		}
+	},
+	//  doesnt work for now
+	// imgo: {
+	// 	icons: {
+	// 		src: 'production/img/*.png'
+	// 	}
+	// },
+	// watch: {
+	// 	files: ['src/sass/*.scss', 'src/*html', 'src/js/*.js'],
+	// 	tasks: ['compass', 'copy', 'includereplace', 'concat', 'csso', 'clean']
+	// },
+	// imagemin?
+	regarde: {
+		compile: {
+			files: ['src/sass/*.sass', 'src/*.html', 'src/js/*.js'],
+			tasks: ['compass', 'copy', 'includereplace', 'concat', 'csso', 'clean:dev','livereload']
+		},
+		reload: {
+			files: ['production/*.*', 'production/**.*'],
+			tasks: [ 'livereload' ]
+		}
+	},
+	clean: {
+		dev: ["production/_*.html", "production/css/lib", "src/css",'production/docs'],
+		release: ["production/*",]
+	}
+	//clean: ["production/_*.html", "production/css/lib", "src/css",'production/docs']
+});
+	//require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-yui-compressor');
+	grunt.loadNpmTasks('grunt-include-replace');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-regarde');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-csso');
+	grunt.loadNpmTasks('grunt-styleguide');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-livereload');
+	grunt.loadNpmTasks('grunt-yui-compressor');
+	// grunt.loadNpmTasks('grunt-imgo');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+
+	grunt.registerTask( 'default', ['livereload-start', 'connect', 'regarde' ]);
+	grunt.registerTask( 'release', ['clean:release', 'compass', 'copy', 'includereplace', 'concat', 'csso', 'min', 'styleguide']);
+
+
+};
---
+++ widthout_grunt/js/common.js
@@ -0,0 +1,350 @@
+$(document).ready(function() {
+
+
+var body = $('body');
+var page = $('.page');
+var wrap = $('.wrap');
+var header = $('.header');
+
+var btn_login = $('.js-login');
+btn_login.click(function() {
+  $(this).next().fadeIn(300);
+});
+btn_login.click(function(event){
+  event.stopPropagation();
+});
+
+var btn_shop = $('.js-shops');
+btn_shop.click(function() {
+  $(this).toggleClass('is-open');
+});
+btn_shop.find('li').click(function() {
+  var pic = $(this).find('img').attr('src');
+  $(this).parent().parent().next().find('img').attr('src', pic);
+});
+btn_shop.click(function(event) {
+  event.stopPropagation();
+});
+
+var btn_sort = $('.js-sort');
+btn_sort.click(function() {
+  $(this).next().fadeIn(300);
+})
+btn_sort.next().find('li').click(function() {
+  var val = $(this).text();
+  $(this).parent().prev().html(val);
+});
+btn_sort.click(function(event) {
+  event.stopPropagation();
+});
+
+var cart_show = $('.js-cart-show');
+cart_show.click(function() {
+  if ($(this).hasClass('is-active')) {
+    $(this).parent().animate({right: -280} , 300);
+    $(this).removeClass('is-active');
+  }
+  else {
+    $(this).parent().animate({right: 20} , 300);
+    $(this).addClass('is-active');
+  };  
+});
+
+var overlay = $('.overlay');
+var cart_el =  $('.cart');
+var cart_popup = $('.cart__popup');
+var cart_close = $('.js-cart-close');
+var cart_popup_open = $('.js-cart-popup > li');
+var cart_arr = $('.cart__arr');
+
+cart_popup_open.click(function() {
+  var wnd_width = $(window).width();  
+  var pos_top = $(this).position().top;
+  var height = $(this).height();
+  var top = pos_top + height/2 - 14;
+  cart_el.addClass('is-open');
+  cart_popup.show();
+  cart_arr.css('top', top);
+  cart_show.hide();  
+  if (wnd_width < 1120) {
+    cart_popup.css({'width': 'auto', 'left': 315 - wnd_width});
+  }
+  else {
+    cart_popup.css({'width': '760px', 'left': 'auto'});
+  } 
+  //scroll
+  $('.js-scroll').jScrollPane( {
+    hideFocus: true
+    //horizontalDragMaxWidth: 60
+  });
+});
+
+cart_close.click(function() {
+  cart_popup.hide();
+  overlay.hide();
+  body.removeClass('no-scroll');
+  if (cart_show.hasClass('is-active')) {
+    cart_show.show();
+  };
+  cart_el.removeClass('is-open');  
+});
+
+overlay.click(function() {
+  $(this).hide();
+  cart_popup.hide();
+  prod.hide();
+  cart_el.removeClass('is-open');
+  body.removeClass('no-scroll');
+  if (cart_show.hasClass('is-active')) {
+    cart_show.show();
+  };
+});
+
+//product popup
+var prod = $('.prod');
+var prod_open = $('.js-prod-open');
+var prod_close = $('.js-prod-close');
+prod_open.click(function() { 
+  var prod_height = prod.height();   
+  body.css({'min-height': prod_height}); 
+  prod.show(); 
+});
+prod_close.click(function() {
+  prod.hide();
+  body.removeClass('no-scroll');
+});
+
+//buy prod (add prod in cart)
+var add_prod = $('.js-add-prod');
+var del_prod = $('.js-del-prod')
+add_prod.click(function() {
+  $(this).parent().parent().animate({left: 0}, 200);
+  $(this).parent().parent().parent().parent().addClass('is-selected');
+});
+del_prod.click(function() {
+  $(this).parent().parent().parent().animate({left: '-100%'}, 200);
+  $(this).parent().parent().parent().parent().parent().removeClass('is-selected');
+});
+
+//page
+var page_fixed = $('.js-prod-open, .js-cart-popup > li');
+var page_no_fixed = $('.js-prod-close, .js-cart-close, .overlay');
+var page_top = 0;
+
+page_fixed.click(function() { 
+  page_top = scroll_top;
+  page.css('top', -page_top);
+  body.addClass('no-scroll');
+  overlay.show();
+});
+
+page_no_fixed.click(function() {
+  overlay.hide();
+  $(window).scrollTo(page_top + 'px', 0);
+  body.removeAttr('style');
+});
+
+$(document).click(function() {
+  btn_login.next().hide();
+  btn_shop.removeClass('is-open'); 
+  btn_sort.next().hide();
+});
+
+//menu - sub
+function submenu() {
+	var el = $('.menu');
+	var el_sub = $('.menu__sub');
+	var item = el.find('.menu__list-in > li');
+  var arr = el.find('.menu__sub-arr');
+	item.hover(		
+    function () {
+    	var item_sub = $(this).attr('data-item');
+      var item_cur = $('#' + item_sub);
+      var width = $(this).width();
+      var width_parent = el.width();
+      var width_sub = item_cur.width(); 
+    	var pos_left = $(this).position().left;    	
+    	var arr_left = pos_left + width/2 - 7;
+      arr.css('left', arr_left);   
+      arr.show(); 	   	    
+      item_cur.hover(
+      	function() {          
+      		$(this).show();
+          arr.show();
+      	},
+      	function() {
+      		$(this).hide();
+          arr.hide();
+      	}
+      );      
+      if (width_parent - width_sub > pos_left) {
+      	item_cur.css({'left': pos_left, 'right': 'auto'}).show();
+    	}
+    	else {
+    		item_cur.css({'left': 'auto', 'right': '-30px'}).show();
+    	}
+    },
+    function() {
+      arr.hide();   
+      el_sub.hide();
+    }
+  );
+};
+//menu - slider
+function slidermenu_responsive() {
+  var el = $('.menu');
+  var prev = el.find('.menu__btn-prev');
+  var next = el.find('.menu__btn-next');
+  var items = el.find('.menu__list-in li');
+  var width_parent = el.width();
+  var width_list = 0;
+  items.each(function() {
+    width_list += $(this).width();
+    return(width_list);
+  });
+  if (width_parent < width_list) {
+    el.addClass('is-slider');
+    next.show();
+  }
+  else {
+    el.removeClass('is-slider');
+    next.hide();
+    prev.hide();
+  }
+};
+function slidermenu() {
+  var el = $('.menu');
+  var el_scroll = el.find('.menu__list');
+  var prev = el.find('.menu__btn-prev');
+  var next = el.find('.menu__btn-next');
+  var items = el.find('.menu__list-in li');
+  el.find('.menu__list-in li:first').addClass('is-current');
+  var width_parent = el.width();
+  var width_list = 0;
+  items.each(function() {
+    width_list += $(this).width();
+    return(width_list);
+  });
+  next.click(function() {
+    prev.show();
+    var width_parent = el.width();
+    el_scroll.scrollTo('+=200px', 400, {
+      onAfter: function() {    
+        var pos_left = el.find('.menu__list-in').position().left;
+        if (pos_left == width_parent - width_list) {     
+          next.hide();
+        };
+      }
+    });
+  });
+  prev.click(function() {
+    next.show();
+    el_scroll.scrollTo('-=200px', 400, {
+      onAfter: function() {    
+        var pos_left = el.find('.menu__list-in').position().left;
+        if (pos_left == 0) {     
+          prev.hide();
+        };
+      }
+    });
+  });
+};
+
+//weight
+function weight() {
+  var el = $('.js-weight');
+  var plus = el.find('.weight__btn-plus');
+  var minus = el.find('.weight__btn-minus');
+  plus.click(function() {
+    var val = $(this).parent().find('.weight__val input').val();
+    var factor = el.parent().find('.weight__val input').attr('data-factor');
+    var sum = parseInt(val) + parseInt(factor);
+    $(this).parent().find('.weight__val input').val(sum);
+  });
+  minus.click(function() {
+    var val = $(this).parent().find('.weight__val input').val();
+    var factor = el.parent().find('.weight__val input').attr('data-factor');
+    var res = parseInt(val) - parseInt(factor);
+    if (res > 0) {
+       $(this).parent().find('.weight__val input').val(res);
+    };   
+  });
+};
+
+//tabs
+function tabs() {
+  var el = $('.js-tabs');
+  var nav_item = el.find('.tabs__nav button');
+  el.find('.tabs__content:not(:first)').hide();
+  nav_item.click(function() {
+    if (!$(this).hasClass('is-active')) {
+      var val = $(this).attr('data-tab');    
+      el.find('.tabs__content').slideUp();
+      nav_item.removeClass('is-active');
+      $(this).addClass('is-active');
+      $('.' + val).slideDown();
+    };
+  });
+};
+
+function cart() {
+  var wnd_width = $(window).width();
+  var cart = $('.cart');
+  cart_show.removeClass('is-active'); 
+  cart_popup.hide();   
+  if (wnd_width < 1120) {
+    cart_show.show();
+    cart.addClass('is-move');
+    cart.css('right', -280);       
+  }
+  else {
+    cart_show.hide();
+    cart.removeClass('is-move');
+    cart.css('right', 20);    
+  };
+};
+
+//main slider
+function m_slider() {
+  var el = $('.js-m-slider');
+  var items = el.find('.m-slider__item').length;
+  var pager = el.find('.m-slider__pager');
+  if (items <= 1) {
+    pager.hide();
+  };
+}
+
+//init
+submenu();
+slidermenu();
+slidermenu_responsive();
+weight();
+tabs();
+cart();
+m_slider();
+
+//window resize
+$(window).resize(function() {
+  //init
+  slidermenu_responsive();
+  cart();
+  $(window).scrollTo(page_top + 'px', 0);
+  body.removeAttr('style');
+});
+
+//window scroll
+var scroll_top = 0;
+$(window).scroll(function() {
+  scroll_top = $(window).scrollTop();  
+  if (!body.hasClass('no-scroll')) {
+    if (scroll_top > 90) {
+      wrap.addClass('wrap_fixed');
+    }
+    else {
+      wrap.removeClass('wrap_fixed');
+    };    
+  };   
+});
+
+});
+
